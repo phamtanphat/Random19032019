a79c84a209fc7cf79809653ce27a08c35317b994
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-04 21:27:03 +0700

    A

--- app/src/main/java/khoapham/ptp/phamtanphat/random1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/random1903/MainActivity.java
@@ -3,20 +3,30 @@ package khoapham.ptp.phamtanphat.random1903;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtSomin,edtSomax;
+    Button btnRandom;
+    TextView txtKetqua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random random = new Random();
-//        (max - min + 1) + min;
-        int value = random.nextInt(2000 - 5 + 1) + 5;
-        Log.d("BBB",value + " " );
-
+//        Random random = new Random();
+////        (max - min + 1) + min;
+//        int value = random.nextInt(2000 - 5 + 1) + 5;
+//        Log.d("BBB",value + " " );
+        edtSomax = findViewById(R.id.edittextSomax);
+        edtSomin = findViewById(R.id.edittextSomin);
+        btnRandom = findViewById(R.id.buttonRandom);
+        txtKetqua = findViewById(R.id.textviewKetqua);
 
     }
 }
